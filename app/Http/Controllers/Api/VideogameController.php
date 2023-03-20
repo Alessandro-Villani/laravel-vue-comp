70e59ffe8f6071d75b41d2488016547c21901e01
Alessandro-Villani <alessvill@libero.it>
2023-03-20 15:58:02 +0100

    first commit

---
+++ app/Http/Controllers/Api/VideogameController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Videogame;
+use Illuminate\Http\Request;
+
+class VideogameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $videogames = Videogame::orderBy('title')->get();
+
+        return response()->json($videogames);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
